dff4b1dd95923633df5c9ff9adad89cd0f512f40
Noah Huppert <noah.programmer@gmail.com>
2018-01-12 13:36:23 -0500

    added parserrunner

---
+++ parsers/parserrun.go
@@ -0,0 +1,92 @@
+package parsers
+
+import (
+	"github.com/Noah-Huppert/crime-map/models"
+)
+
+// ParserRunner invokes a series of Parsers on an array of fields. Fields will
+// parsed be parsed in order. Parsers will be invoked on these fields in a
+// specified order.
+//
+// The next field will be parsed when any of the Parsers indicates that it
+// consumed one or more fields.
+//
+// These Parsers can be added in the order they will be invoked.
+type ParserRunner struct {
+	// parsers holds the Parsers used to extract information from a list
+	// of fields. Parsers in this slice will be tried one after the next
+	// until a field is successfully parsed. Then the next field will be
+	// parsed.
+	//
+	// The order of Parsers in this slice indicates precedence.
+	parsers []Parser
+}
+
+// NewParserRunner creates and returns a ParserRunner reference
+func NewParserRunner() *ParserRunner {
+	return &ParserRunner{
+		parsers: []Parser{},
+	}
+}
+
+// Parse will parse the provided fields with the previously added
+// Parsers.
+//
+// The Crimes which were parsed from these fields will be returned.
+// Along with any error that occurs. Nil on success.
+func (r ParserRunner) Parse(fields []string) ([]*models.Crime, error) {
+	// crimes holds the Crime models parsed by the parsers
+	crimes := []*models.Crime{}
+
+	// crime is the Crime model currently being parsed
+	crime := &models.Crime{}
+
+	// report is the Report model currently being parsed
+	report := &models.Report{}
+
+	// Loop through fields and parse
+	for fI, field := range fields {
+		// Run one parser after another until a field is successfully
+		// parsed. Then move to the next field until done.
+		for pI, parser := range r.parsers {
+			delta, err := parser.Parse(i, fields, report, crime)
+
+			// Check if current crime has been successfully parsed
+			if err == ErrCrimeParsed {
+				// Add current crime to list
+				crimes = append(crimes, crime)
+
+				// Make new crime model
+				crime := &models.Crime{}
+			} else if err != nil {
+				// If other error
+				return crimes, fmt.Errorf("error running %s parser "+
+					"against field with index %d, err: %s", parser,
+					fI, err.Error())
+			}
+
+			// Check if any fields were parsed
+			if delta > 0 {
+				// Increment field index
+				i += delta
+
+				// Go to next field
+				break
+			}
+		}
+
+		// If not parsed, error
+		return crimes, fmt.Errorf("no parser parsed field with index"+
+			" %d, field: %s", fI, field)
+	}
+
+	// Success
+	return crimes, nil
+}
+
+// Add will save the provided parser for use when any fields need to
+// be parsed.
+func (r *ParserRunner) Add(parser Parser) {
+	r.parsers = append(r.parsers, parser)
+	return nil
+}
