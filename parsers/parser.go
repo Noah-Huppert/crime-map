be6b64fca63d6eff7478961d85e7b0d566ffa63e
Noah Huppert <noah.programmer@gmail.com>
2018-01-12 12:03:57 -0500

    defined more simple parser iface

--- parsers/parser.go
+++ parsers/parser.go
@@ -10,6 +10,7 @@ import (
 
 // Parser provides methods for converting a stream of pdf text fields to Crime
 // structs. This allows multiple different formats of reports to parsed.
+/*
 type Parser interface {
 	// Parse takes the text fields saved in the implementing struct and
 	// parses them into a slice of Crime structs.
@@ -31,6 +32,29 @@ type Parser interface {
 	// Count returns the number of Crime models parsed from a report. An
 	// error is returned if one occurs. Nil on success.
 	Count() (uint, error)
+}
+*/
+// Parser takes a single text field and extracts information related to
+// the general crime report document or a single reported crime.
+//
+// Parser implementations should each only handle 1 type of data field. Multiple
+// Parsers can then be run together to handle every type of text field a crime
+// or report may contain.
+type Parser interface {
+	// Parse extracts information from a text field contained in a crime
+	// report pdf.
+	//
+	// The index of the field to parse is provided by the `i` argument. The
+	// slice of fields to access is provided by the `fields` argument.
+	//
+	// Information which is parsed should be saved in the models.Report and
+	// models.Crime pointers.
+	//
+	// The number of fields which were parsed should be returned. 0 if no
+	// fields were parsed. This value will be added to the current field
+	// index (`i`) for the next invocation of the parser.
+	// Additionally an error is returned if one occurs, nil on success.
+	Parse(i uint, fields []string, report *models.Report, crime *models.Crime) (uint, error)
 }
 
 // determineUniversity figures out which University a crime report was
